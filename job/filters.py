a89202a4e9f3dc15686549836f32d77ccceeb247
jalaljamalXD <noob123noob123jjxd@gmail.com>
2022-09-28 04:03:25 -0700

    search, pagination

--- job/filters.py
+++ job/filters.py
@@ -4,8 +4,12 @@ from .models import Job
 
 class JobsFilter(filters.FilterSet):
 
+    keyword = filters.CharFilter(field_name='title', lookup_expr='icontains')
+    location = filters.CharFilter(field_name='address', lookup_expr='icontains')
     min_salary = filters.NumberFilter(field_name="salary" or 0, lookup_expr='gte')
+    max_salary = filters.NumberFilter(field_name="salary" or 1000000, lookup_expr='lte')
+
 
     class Meta:
         model = Job
-        fields = ('education', 'jobType', 'experience')
+        fields = ('keyword','location','education', 'jobType', 'experience', 'min_salary', 'max_salary')
