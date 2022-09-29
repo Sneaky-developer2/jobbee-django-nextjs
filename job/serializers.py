763ec239b448a736107a2e4b5e445b9d92bf9050
jalaljamalXD <noob123noob123jjxd@gmail.com>
2022-09-29 04:26:51 -0700

    CondidatesAppliedSerializer

--- job/serializers.py
+++ job/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 
-from .models import Job
+from .models import CondidatesApplied, Job
 
 
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = '__all__'
-        
+
+
+class CondidatesAppliedSerializer(serializers.ModelSerializer):
+
+    job = JobSerializer
+
+    class Meta:
+        model = CondidatesApplied
+        fields = ('user', 'resume', 'appliedAt', 'job')
